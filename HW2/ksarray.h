7a729f62b618ba7b29a3ec47648fabf9b2a08a55
nrwade <nrwade@alaska.edu>
2012-10-03 01:40:26 -0800

    almost there

--- HW2/ksarray.h
+++ HW2/ksarray.h
@@ -1,282 +1,206 @@
-
 #ifndef KSARRAY_H
 #define KSARRAY_H
 
 #include <algorithm>
 
-// Write class invariants
-// Write pre and post conditions AND requirements on types for all templates
+#include <iostream>
+using std::ostream;
 
-//No resource leaks!
-
-//Document ownership and related issues
-
-//pre-declarations so it knows our friends
-/*
-template<typename AType> class KSArray; //pre-declare the class itself
-template<typename AType> bool operator== (const KSArray<AType>& lhs, const KSArray<AType>& rhs);
-template<typename AType> bool operator < (const KSArray<AType>& lhs, const KSArray<AType>& rhs);
-*/
-
-//AType is the type of all the items stored in the container
-template <typename AType> 
+template <class at>
 class KSArray
 {
+
 public:
-	typedef AType value_type; 
-	// typedef allows us to use the template parameter outside of the class definition
-	// this lets us say "value_type" when we mean the type of the items in the container
-
-	typedef AType size_type;
-	//the type of the size of the array
-	//I don't really understand why we need this... humph
-
+	typedef at value_type; 
+	typedef std::size_t size_type;
 	//default constructor
 	KSArray();
-	
-	//constructor 1-parameter
-	KSArray(size_type numItems);
-
-	//constructor 2-parameter
-	KSArray(size_type numItems, value_type val);
-
+	//single parameter constructor
+	KSArray(size_type theSize);
+	//two parameter constructor
+	KSArray(value_type param, size_type theSize);
 	//copy constructor
 	KSArray(const KSArray & toCopy);
-
-	size_type size();
-
+	//retrieve the size 
+	size_type size() const;
+	//begin iterator
 	value_type *begin();
+	//constant begin iterator
 	const value_type *begin() const;
-
+	//end iterator
 	value_type *end();
+	//constant end iterator
 	const value_type *end() const;
-
+	//destructor
 	~KSArray();
-
-//~~~~~~~~~~~~~~~~~~~~ERROR~~~~~~~~~~~~~~~~~~~~~~~~~~ 
-/*
-ksarray.h:54: error: template-id ‘operator==<double>’ for ‘bool operator==(const KSArray<double>&, const KSArray<double>&)’ does not match any template declaration
-ksarray.h:55: error: template-id ‘operator< <double>’ for ‘bool operator<(const KSArray<double>&, const KSArray<double>&)’ does not match any template declaration
-*/	
-	friend bool operator== <AType>(const KSArray<AType>& lhs, const KSArray<AType>& rhs);
-	friend bool operator < <AType>(const KSArray<AType>& lhs, const KSArray<AType>& rhs);
+	//bracket operator
 	value_type & operator [] (size_type index);
+	//costant bracket operator
 	const value_type & operator [] (size_type index) const;
-//need to make a friend copy assignment
+	//overloaded = operator
+	KSArray & operator=(const KSArray & rhs);
 
 private:
 	value_type * _arrayPtr; // pointer to a value_type
+	size_type _arraySize; //hold the array size 
 		
 };
 
-//default constructor
-template<typename AType>
-KSArray<AType>::KSArray():_arrayPtr(new value_type[10])
+template <class at>
+KSArray<at>::KSArray():_arrayPtr(new value_type[10]), _arraySize(10)
 {}
 
-// should create a KSArray of size 10
-	// all items in the array should be default constructed 
-	
-	//constructor 1-parameter
-template<typename AType>
-KSArray<AType>::KSArray(size_type numItems)
-{
-	//USE AN INITIALIZER LIST
-	_arrayPtr = new value_type[numItems];
-}
-	// parameter is a non-negative integer giving the number of items in the array
-	// all items in the array should be default constructed
-	//may not create any new implicit type conversions
+template <class at>
+KSArray<at>::KSArray(size_type theSize)
+	:_arrayPtr(new value_type[theSize]), _arraySize(theSize) 
+{}
 
-	//constructor 2-parameter
-//~~~~~~~~~~~~~~~~~~~~~~ERROR~~~~~~~~~~~~~~~~~~~~~~~~~
-/*
-ksarray.h:92: error: size in array new must have integral type
-ksarray.h: In constructor ‘KSArray<AType>::KSArray(AType, AType) [with AType = double]’:
-*/
-template<typename AType>
-KSArray<AType>::KSArray(size_type numItems, value_type val)
+template <class at>
+KSArray<at>::KSArray(value_type val, size_type theSize)
+	:_arrayPtr(new value_type[theSize]), _arraySize(theSize)
 {
-	_arrayPtr = new value_type[numItems];
-	//use our iterators begin and end to loop through and set each item in the array to val
-	for(value_type * iter=begin(); iter<=end(); iter++)//do I need to put KSArray.begin() and end? <= or < end
+	for(int i=0; i<_arraySize; i++)
 	{
-		*iter=val; //we dereference the pointer and set it to val
+		_arrayPtr[i] = val;
 	}
 }
-	// first parameter is a non-negative integer giving the number of items in the array
-	// second parameter is an item of the value type- each item in the array should be set to this value
+
 
 //copy constructor
-template<typename AType>
-KSArray<AType>::KSArray(const KSArray & toCopy)
-	:_arrayPtr(new value_type[toCopy.size()]) 
-	{
-		std::copy(toCopy.begin(), toCopy.end(), _arrayPtr);
-
-	}
-	// must take a const 
-	// creates a new object
-
-
-//operator=
-//WRITE THIS!	
-	// sets an existing object equal to a copy of some other existing object
-// check for self-assignment. If this is not self-assignment, deallocate the old array nd then do essentially what the copy ctor does
-// return the object assigned. See slide 11 for sample code
-
-template<typename AType>
-typename KSArray<AType>::size_type KSArray<AType>::size()	//returns the number of items in the array
+template< class at>
+KSArray<at>::KSArray(const KSArray & toCopy)
+	:_arrayPtr(new value_type[toCopy.size()]), _arraySize(toCopy.size()) 
 {
-	size_type count=0;
-	for (value_type * iter=begin(); iter<=end(); iter++)
-	{
-		count +=count;
-	}
-	return(count);
+	std::copy(toCopy.begin(), toCopy.end(), _arrayPtr);
+} 
+
+template< class at>
+typename KSArray<at>::size_type KSArray<at>::size() const
+{
+	return(_arraySize);
 }
 
-
-// the two functions should be essentially identical except for the const at the end of the first line
-// and the return method
-// or we may use the const_cast trick to call the const version from the non-const version
-template<typename AType>
-typename KSArray<AType>::value_type * KSArray<AType>::begin()
+template< class at>
+typename KSArray<at>::value_type * KSArray<at>::begin()
 {
 	return(_arrayPtr);
 }
 
-//~~~~~~~~~~~~~~~~~~~~~~~~~~ERROR~~~~~~~~~~~~~~~~~~~~~~
-/*
-ksarray.h:149: error: invalid operands of types ‘double*’ and ‘double’ to binary ‘operator+’
-*/
-template<typename AType>
-typename KSArray<AType>::value_type * KSArray<AType>::end()
+template< class at>
+const typename KSArray<at>::value_type * KSArray<at>::begin() const
 {
-	return(begin() + size()+1);//plus one because we need one past the end
+	return(_arrayPtr);
+}//not sure how to check that this works
+
+template< class at>
+typename KSArray<at>::value_type * KSArray<at>::end()
+{	
+	return(begin()+_arraySize);
 }
-//I think I'm doing a type conversion here that I shouldn't be
-//ksarray.h:151: error: invalid operands of types ‘double*’ and ‘double’ to binary ‘operator+’
 
-
-
-
-template<typename AType>
-const typename KSArray<AType>::value_type *KSArray<AType>::end() const
-{
-	return(begin() + size()+1);
+template< class at>
+const typename KSArray<at>::value_type * KSArray<at>::end() const
+{	
+	return(begin()+_arraySize);
 }
-	// returns an iterator to just past the last array item- can be a pointer
-	// add a number to the return value of begin- what number? size()
-	// returns the address of the item one-past the end of the array (think iterator)
 
 //destructor
-template<typename AType>
-KSArray<AType>::~KSArray()
+template<class at>
+KSArray<at>::~KSArray()
 {
-	delete _arrayPtr;
+	delete [] _arrayPtr;
 }
-	// deallocate all memory- aka delete all pointers!
 
-template <typename AType> 
-bool operator==(const KSArray<AType>& lhs, const KSArray<AType>& rhs)
+template <class at>
+ typename KSArray<at>::value_type & KSArray<at>::operator [] (typename KSArray::size_type index)
+ {
+ 	if (0 <= index < _arraySize)
+ 	{
+ 	return (_arrayPtr[index]);//following the example, but does this actually return a reference?
+ 	}
+ }
+
+ template <class at>
+ const typename KSArray<at>::value_type & KSArray<at>::operator [] (typename KSArray::size_type index) const
+ {
+ 	if (0 <= index < _arraySize)
+ 	{
+ 	return (_arrayPtr[index]);//following the example, but does this actually return a reference?
+ 	}
+ }
+
+template <class at>
+KSArray<at> & KSArray<at>::operator=(const KSArray<at> & rhs)
+{
+	if (this != &rhs)
+	{
+		delete [] _arrayPtr;
+		_arrayPtr = new value_type[rhs.size()]; 
+		_arraySize = rhs.size(); 
+		std::copy(rhs.begin(), rhs.end(), _arrayPtr);
+	}
+	return *this;
+}
+
+template <class at> 
+bool operator==(const KSArray<at>& lhs, const KSArray<at>& rhs)
 {	
 	bool comp = false;
 
-	if (lhs.value_type == rhs.value_type && lhs.size() == rhs.size())
+	if (lhs.size() == rhs.size())
 	{
-		for (typename KSArray<AType>::value_type * iter=lhs.begin(); iter<=lhs.end(); iter++)
+		for (int i=0; i< lhs.size(); i++)
 		{
-			if (lhs._arrayPtr+iter == rhs._arrayPtr+iter)
-				{
-					comp=true;
-				}
-			else
+			if (lhs[i] == rhs[i])
 			{
-				comp=false;
+				comp = true;
 			}
 		}
 	}
 
-	return(lhs.value_type == rhs.value_type && lhs.size() == rhs.size() && comp);
+	return(comp);
 
 }
 
-
-// two KSArray with the same value type are equal if they have the same size and their corresponding 
-// items are all equal.
-// Two KSArrays with differnt value types cannot be compared
-// Does this need to be a friend to the class?
-template <typename AType> 
-bool operator != (const KSArray<AType>& lhs, const KSArray<AType>& rhs)
+template <class at> 
+bool operator != (const KSArray<at>& lhs, const KSArray<at>& rhs)
 {
 	return(! (lhs==rhs) );
 }
-// two KSArray with the same value type are equal if they have the same size and their corresponding 
-// items are all equal.
-// Two KSArrays with different value types cannot be compared 
 
-
-template <typename AType>
-bool operator<(const KSArray<AType>& lhs, const KSArray<AType>& rhs)
+template <class at>
+bool operator<(const KSArray<at>& lhs, const KSArray<at>& rhs)
 {
 	bool comp=false;
 
-	if (lhs.value_type == rhs.value_type)
+	for (int i=0; i<lhs.size(); i++)
 	{
-		for(typename KSArray<AType>::value_type * iter=lhs.begin(); iter <= lhs.end(); iter++)
+		if (lhs[i] < rhs[i])
 		{
-			if (lhs._arrayPtr < rhs._arrayPtr)
-			{
-				comp=true;
-			}
-			else
-			{
-				comp=false;
-			}
+			comp = true;
 		}
 	}
+
 	return comp;
 }
 
-
-
-//So if we just us == and < and those are templates, do these need to be templates?
-template <typename AType>
-bool operator<=(const KSArray<AType>& lhs, const KSArray<AType>& rhs)
+template <class at>
+bool operator<=(const KSArray<at>& lhs, const KSArray<at>& rhs)
 {
 	return(!(rhs<lhs));
 }
 
-
-template <typename AType>
-bool operator>(const KSArray<AType>& lhs, const KSArray<AType>& rhs)
+template <class at>
+bool operator>(const KSArray<at>& lhs, const KSArray<at>& rhs)
 {
 	return(!(lhs<=rhs));
 }
 
-template <typename AType>
-bool operator>=(const KSArray<AType>& lhs, const KSArray<AType>& rhs)
+template <class at>
+bool operator>=(const KSArray<at>& lhs, const KSArray<at>& rhs)
 {
 	return(!(lhs<rhs));
 }
-//check that all of the operators are consistent
 
-//copy assignment
-
-template <typename AType>
- typename KSArray<AType>::value_type & KSArray<AType>::operator [] (typename KSArray<AType>::size_type index)
- {
- 	return (_arrayPtr[index]);
- }
-
-template <typename AType>
- const typename KSArray<AType>::value_type & KSArray<AType>::operator [] (typename KSArray<AType>::size_type index) const
- {
- 	return (_arrayPtr[index]);
- }
-
-
-
-
-#endif
+//KSARRAY_H
+#endif  
--- HW2/main.cpp
+++ HW2/main.cpp
@@ -3,23 +3,91 @@ using namespace std;
 
 //#include "ksarray2.h"
 
-#include "KSArray3.h"
+#include "ksarray.h"
 
 int main() {
 	
 	cout << "HELL OWORD\n";
+
+	const int theSize=1000;
+	const double val = -3.2;
+
+    KSArray<double> tad(theSize, val);
 	
-	KSArray<int> ia = KSArray<int>(); 
+	//KSArray<int> ia = KSArray<int>(); 
 
-	KSArray<int> ia2 = KSArray<int>(2); 
+	//KSArray<int> ia2 = KSArray<int>(2); 
 
-	KSArray<char> ia3 = KSArray<char>('a'); 
+	/*KSArray<char> ia3 = KSArray<char>('a'); 
 
 	KSArray<double> ia4 = KSArray<double>(5.6, 4);
+	KSArray<double> ia7 = KSArray<double>(5.2, 4);
 
 	KSArray<char> ia5 = KSArray<char>('l', 4);
 
 	KSArray<int> ia6 = KSArray<int>();
+
+	KSArray<char> ia8 = KSArray<char>('a', 4);
+	KSArray<char> ia9 = KSArray<char>('b', 5);
+
+	 KSArray<int> ia(10);          // array of 10 ints
+  KSArray<int> iax;             // another array of 10 ints
+  KSArray<double> da(40, 3.2);  // array of 40 doubles, each of which is set to 3.2
+  //KSArray x(10);                // WILL NOT COMPILE; no template parameter
+  
+  // Set all items (counter loop)
+  for (int c = 0; c < ia.size(); ++c)
+  {
+      ia[c] = c * c;
+  }
+  
+  // Print all items (iterator loop)
+  const int * iter;
+  for (iter = ia.begin(); iter != ia.end(); ++iter)
+  {
+      cout << "Item :" << *iter << endl;
+  }
+  
+  const KSArray<int> ia2(ia);   // Copy constructor
+  if (ia2 == ia)                // Condition should be true
+      cout << "Equal!" << endl;
+  
+  KSArray<double> da2;
+  da2 = da;                     // Copy assignment
+  //da2 = ia;                     // WILL NOT COMPILE; different types
+  
+  //if (da == ia)                 // WILL NOT COMPILE; different types
+     // cout << "ZZZ" << endl; */
+
+
+	/*cout << "ia8 " << ia8.size() << " " << *ia8.begin() << endl;
+	cout << "ia9 " << ia9.size() << " " << *ia9.begin() << endl;
+	ia8= ia9;
+
+	cout << "ia8 has been set to ia 9 \n";
+	cout << "ia8 " << ia8.size() << " " << *ia8.begin() << endl;
+	cout << "ia9 " << ia9.size() << " " << *ia9.begin() << endl;*/
+
+	/*int x=4;
+	int y=5;
+	cout << "x= " << x << endl;
+	cout << "y= " << y << endl;
+
+	x=y;
+	cout << "x= " << x << endl;
+	cout << "y= " << y << endl;*/
+
+	/*cout << "ia9 " << ia9.size() << " "<< *ia9.begin()<< endl;
+	cout << "ia10 " << ia10.size() << " " << *ia10.begin() << endl;
+
+	if (ia10 == ia9)
+	{
+		cout << "success" << endl;
+	}
+	else
+	{
+		cout << "fuuuuck" << endl;
+	}*/
 
 	/*cout<< "the size of array ia5 is "<<ia5.size() << endl;
 
@@ -31,15 +99,15 @@ int main() {
 	/*cout << "size of ia " << ia.size() << endl;
 	cout << "size of ia6 " << ia6.size()<< endl;
 	cout << "size of ia4 " << ia4.size()<<endl;*/
-	if (ia6==ia2)
+	/*if (ia9>=ia8)
 	{
-		cout << "they are equal" << endl;
+		cout << "ia9 >= ia8" << endl;
 	}
 
 	else 
 	{
-		cout << "not equal" << endl;
-	}
+		cout << "ia9 !>= ia8" << endl;
+	}*/
 
 	return 0;
 }
